300fa56d590f77408c438f185eb24be773835565
Christopher Little <hephistocles@gmail.com>
2016-05-05 22:39:23 +0100

    Add cypher timer

--- src/main/scala/re/toph/hybrid_db/HelloWorld.scala
+++ src/main/scala/re/toph/hybrid_db/HelloWorld.scala
@@ -20,46 +20,25 @@ object HelloWorld {
 
   Class.forName(driver)
   implicit val connection:Connection =  DriverManager.getConnection(url, user, pass)
+  implicit val wsclient = ning.NingWSClient()
+  implicit val connection2 = Neo4jREST("localhost", 7474, "neo4j", pass)
+
+  // Provide an ExecutionContext
+  implicit val ec = scala.concurrent.ExecutionContext.global
 
   def main(args: Array[String]): Unit = {
 
-    // Provide an instance of WSClient
-    //    val wsclient = ning.NingWSClient()
-    implicit val wsclient = ning.NingWSClient()
-    try {
-
-      // Setup the Rest Client
-      //    implicit val connection = Neo4jREST()(wsclient)
-      implicit val connection2 = Neo4jREST("localhost", 7474, "neo4j", "LockDown1")
-
-      // Provide an ExecutionContext
-      implicit val ec = scala.concurrent.ExecutionContext.global
-
-      // a simple query
-      val req = Cypher("match (p:Point {id:12}) RETURN p")
-
-      // get a stream of results back
-      val stream = req()
-
-      // get the results and put them into a list
-      stream.foreach(println)
-      //    println(stream.map(row => {row[String]("n.name")}).toList)
-    } catch {
-      case x:Throwable => println("Nope")
-    } finally {
-      wsclient.close()
-    }
-
-
-
-    // shut down WSClient
-    val iterations = 3
+    val iterations = 5
     var res = List[Map[String, Any]]()
 
     for (it <- 1 to iterations) {
 
+      Timer.time("Cypher", {
+        getHopDistsCypher(4, 20)(connection)
+      })
+
       Timer.time("SQL Joins", {
-        getHopDistsSQL(connection)
+        getHopDistsSQL(4, 20)(connection)
       })
 
       //    res = Timer.time("No prefetch", {
@@ -67,44 +46,47 @@ object HelloWorld {
       //    })
 
       res = Timer.time("Lookahead Join (1)", {
-        getHopDists(5, 20, new LookaheadJoinPrefetcher(1))
+        getHopDists(4, 20, new LookaheadJoinPrefetcher(1))
       })
 
       for (i <- 3 to 5) {
         res = Timer.time("Lookahead Multi (" + i + ")", {
-          getHopDists(4, 1, new LookaheadMultiPrefetcher(i))
+          getHopDists(4, 20, new LookaheadMultiPrefetcher(i))
         })
       }
     }
-//    res.foreach(println)
+
+    // clean up
+    wsclient.close()
+
   }
 
-  def getHopDistsSQL2(connection: Connection) = {
-    val s = connection.createStatement()
-    val rs = s.executeQuery(
-      """
-        |SELECT A.id1 s, D.id2 e,
-        |   4 l, CONCAT(A.id1, '->', B.id1, '->', C.id1, '->', C.id2, '->', D.id2) p, A.dist + B.dist + C.dist + D.dist dist
-        | FROM edges A
-        | JOIN edges B
-        |   ON A.id2 = B.id1
-        | JOIN edges C
-        |   ON B.id2 = C.id1
-        | JOIN edges D
-        |   ON C.id2 = D.id1
-        | JOIN points nA ON A.id1=nA.id
-        | JOIN points nB ON B.id1=nB.id
-        | JOIN points nC ON C.id1=nC.id
-        | JOIN points nD ON D.id1=nD.id
-        | JOIN points nE ON D.id2=nE.id
-        | WHERE A.id1 = 1
-      """.stripMargin)
-    //    while (rs.next()) {
-    //      println(rs.getString("p"))
-    //    }
+  def getHopDistsCypher(hops:Int, n:Long)(implicit connection: Connection) = {
+    Cypher(
+      """MATCH path=(p:Point {id:{id}})-[r:Road*0..""" + hops + """]->(p2)
+        |WITH p, p2, extract(x in r| x.dist) as dists,
+        |length(path) as l
+        |RETURN p.id as s, p2.id as e, l,
+        |reduce(res=0, x in dists | res + x) as dist""".stripMargin)
+    .on("id" -> n, "hops" -> hops)
+    .apply()
+//    .foreach(println)
+      .map(row =>
+        Map(
+          "s"->row[Option[Long]]("s"),
+          "e"->row[Option[Long]]("e"),
+          "l"->row[Option[Int]]("l"),
+//          "p"->row[String]("p"),
+          "dist"->row[Option[Long]]("dist")
+        )).toList
   }
 
-  def getHopDistsSQL(implicit connection: Connection) : List[Map[String, Any]] = {
+//  MATCH path=(p:Point {id:1})-[r:Road*0..3]->(p2)
+//  WITH extract(x in r: x.dist) as dists, extract(x in r:x.id2), length(p) as l
+//  RETURN p.id as s, p2.id as e, l, reduce(res=0, x in scores: res + x) as dist
+
+  def getHopDistsSQL(hops:Int, n:Long)(implicit connection: Connection) : List[Map[String, Any]] = {
+    if (hops != 4) throw new Exception("We only support 4 hops!")
      SQL(
       """
         |SELECT A.id1 s, D.id2 e,
@@ -121,8 +103,10 @@ object HelloWorld {
         | JOIN points nC ON C.id1=nC.id
         | JOIN points nD ON D.id1=nD.id
         | JOIN points nE ON D.id2=nE.id
-        | WHERE A.id1 = 1
-      """.stripMargin)()
+        | WHERE A.id1 = {id}
+      """.stripMargin)
+    .on("id"->n, "hops" -> hops)
+    .apply()
        .map(row =>
          Map(
            "s"->row[Long]("s"),
@@ -131,7 +115,6 @@ object HelloWorld {
            "p"->row[String]("p"),
            "dist"->row[Long]("dist")
          )).toList
-//      .on("countryCode" -> 1).as(SqlParser.int("foo").single)
   }
 
   /*
